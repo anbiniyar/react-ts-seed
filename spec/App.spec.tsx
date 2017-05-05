94f464d5115b120045971b615d3fd9390e0c1d6b
sferrka <sferrrka@gmail.com>
2017-05-05 20:24:18 +0700

    removed yarn because of karma wrong installation , added chai ,sinon

--- spec/App.spec.tsx
+++ spec/App.spec.tsx
@@ -1,12 +1,19 @@
+import { assert } from "chai";
 import { mount, render, shallow } from "enzyme";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as sinon from "sinon";
 
 import App from "./../src/App";
 
 describe("AppComponent", () => {
 
   describe("A suite", () => {
+
+    it("should expose the Chai assert method", () => {
+      assert.ok("everything", "everything is ok");
+    });
+
     it("contains h2 header", () => {
       expect(shallow(<App />).contains(<h2>Welcome to React</h2>)).toBe(true);
     });
@@ -26,5 +33,34 @@ describe("AppComponent", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
+  });
+
+  describe("sinon timer", () => {
+    beforeEach(() => {
+      this.sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(() => {
+      this.sandbox.restore();
+    });
+
+    it("test ticks", () => {
+      function testSinonFakeTimersWith(interval: number, ticks: number) {
+        const clock = sinon.useFakeTimers();
+        const spy = sinon.spy();
+        const id = setInterval(spy, interval);
+        assert(!spy.calledOnce);
+        clock.tick(ticks);
+        assert(spy.callCount === Math.floor(ticks / interval));
+
+        clearInterval(id);
+        clock.restore();
+      }
+
+      testSinonFakeTimersWith(10, 101);
+      testSinonFakeTimersWith(99, 101);
+      testSinonFakeTimersWith(100, 200);
+
+    });
   });
 });
